a341b060e7b1cd1e9b8804d6289d8a2cf40059bb
birajanayak10 <nbiraja916@gmail.com>
2025-06-09 18:41:24 +0530

    changes has  been made

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React,{useContext, useEffect, useState} from 'react'
 import { useParams} from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 
 const Appointment = () => {
@@ -160,6 +161,8 @@ const Appointment = () => {
 </div>
 <button className='bg-primary textwhite text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
     </div>
+    {/* Listing related Doctors */}
+    <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
